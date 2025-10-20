2aa19388a6e1f856702a9873f6819deffe6ed6dd
SenjuroKanez <umershqhzad6@gmail.com>
2025-10-20 23:01:50 +0500

    Fixed binary one count issue

--- Cpp/DSALab manual3 q3.cpp
+++ Cpp/DSALab manual3 q3.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 using namespace std;
 
-//Binary search pehle 1 ko find kr rha he.(╹ڡ╹ )
+// Binary search pehle 1 ko find kr rha he.(╹ڡ╹ )
 int binarySearch(int arr[], int left, int right, int target) {
     if (left > right) {
         return -1; 
@@ -10,9 +10,8 @@ int binarySearch(int arr[], int left, int right, int target) {
 
     int mid = left + (right - left) / 2;
 
-    if (arr[mid] == target && arr[mid-1] == 0 ){
-        return mid; 
-        
+    if (arr[mid] == target && (mid == 0 || arr[mid - 1] == 0)) {
+        return mid;
     } else if (target < arr[mid]) {
         return binarySearch(arr, left, mid - 1, target);
     } else {
@@ -20,25 +19,25 @@ int binarySearch(int arr[], int left, int right, int target) {
     }
 }
 
-//Is function me array input or 1s count ho jaye ga (づ￣ 3￣)づ
-
 int main() {
     int n, l;
-    cout << "Enter the size of the array: ";
+    cout << "\nEnter the size of the array: ";
     cin >> n;
 
     if (n <= 0) {
-        throw std::invalid_argument("Array size must be positive.");
+        throw invalid_argument("Array size must be positive.");
     }
 
     int arr[n];
     cout << "Enter " << n << " elements (0s and 1s only): " << endl;
-    for (int i = 0; i < n; i++) {
+    int i = 0;
+    while (i < n) {
         cin >> l;
         if (l != 0 && l != 1) {
             cout << "Invalid input. Please enter only 0s and 1s." << endl;
-        }else{
+        } else {
             arr[i] = l;
+            i++;
         }
     }
 
@@ -46,20 +45,23 @@ int main() {
     for (int i = 0; i < n; i++) {
         for (int j = i + 1; j < n; j++) {
             if (arr[i] > arr[j]) {
-                int a = arr[i];
+                int temp = arr[i];
                 arr[i] = arr[j];
-                arr[j] = a; 
+                arr[j] = temp;
             }
         }
     }
 
-    int target =1;
+    int target = 1;
     int otal = binarySearch(arr, 0, n - 1, target);
 
-
-    int total = n - otal; 
+    int total;
+    if (otal == -1) {
+        total = 0;
+    } else {
+        total = n - otal;
+    }
 
     cout << "Total number of 1s: " << total << endl;
-
     return 0;
 }
