5e8fe6a440d8fcd34c718b3332e29ba081e70a81
lulguy <24-ai-062@student.hitecuni.edu.pk>
2025-10-22 10:23:40 +0500

    bubble not going to max fixesd

--- Cpp/Sorting ALgo/BubbleSort.cpp
+++ Cpp/Sorting ALgo/BubbleSort.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 void bubbleSort(int arr[], int n){
-    for(int j = 0; j < n - 1; j++){
+    for(int j = 0; j < n ; j++){
         bool swapped = false;
         for(int i = 0; i < n - j - 1 ; i++){
             if(arr[i] > arr[i + 1]){
