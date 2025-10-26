073dc2ee2989dc8f7c07639b46e5f73e37e7ca09
SenjuroKanez <umershqhzad6@gmail.com>
2025-10-27 00:21:17 +0500

    funally a md with sense

--- Cpp/Sorting ALgo/MergeSort.cpp
+++ Cpp/Sorting ALgo/MergeSort.cpp
@@ -38,58 +38,10 @@ void merge(int arr[], int l, int m, int r) {
 void mergeSortp(int arr[], int l, int r) {
     if (l < r) {
         int m = l + (r - l) / 2;
-        mergeSort(arr, l, m);
-        mergeSort(arr, m + 1, r);
+        mergeSortp(arr, l, m);
+        mergeSortp(arr, m + 1, r);
         merge(arr, l, m, r);
     }
-}
-
-
-void mergeSort(int arr[], int l, int r){
-    if(l < r){
-        int m = l + (r - l) / 2;
-
-        mergeSort(arr, l, m);
-        mergeSort(arr, m + 1, r);
-
-        int n1 = m - l + 1;
-        int n2 = r - m;
-
-        int* L = new int[n1];
-        int* R = new int[n2];
-
-        for(int i = 0; i < n1; i++)
-            L[i] = arr[l + i];
-        for(int j = 0; j < n2; j++)
-            R[j] = arr[m + 1 + j];
-
-        int i = 0, j = 0, k = l;
-        while(i < n1 && j < n2){
-            if(L[i] <= R[j]){
-                arr[k] = L[i];
-                i++;
-            } else {
-                arr[k] = R[j];
-                j++;
-            }
-            k++;
-        }
-
-        while(i < n1){
-            arr[k] = L[i];
-            i++;
-            k++;
-        }
-
-        while(j < n2){
-            arr[k] = R[j];
-            j++;
-            k++;
-        }
-
-        delete[] L;
-        delete[] R;
-    }   
 }
 
 int main(){
@@ -102,9 +54,6 @@ int main(){
         cin >> arr[i];
     }
 
-    mergeSort(arr, 0, n - 1);
-    int arr[] = {5, 2, 8, 1};
-    int n = sizeof(arr) / sizeof(arr[0]);
     mergeSortp(arr, 0, n - 1);
     for (int i = 0; i < n; i++)
         cout << arr[i] << " ";
