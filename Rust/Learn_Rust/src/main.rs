a0e4934e9bf7b2b2f59bf52ca0e3bd3a1658e1dd
alternateadmi <24-ai-062@student.hitecuni.edu.pk>
2026-05-13 21:08:11 +0500

    rusting it out

---
+++ Rust/Learn_Rust/src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    println!("Hello, world!");
+    let c = 44;
+    let mut s = 3;
+    println!("c is {c} and s is {s}");
+    s = 4;
+    println!("s is now {s}");
+    let mut ss = "gummplemented!()";
+    println!("{ss}");
+    ss = "pigass";
+    println!("{ss}");
+    let spaces = "   ";
+    let spaces = spaces.len();
+    println!("spaces are {spaces}");
+    let c = 3;
+    println!("{c}");
+}
